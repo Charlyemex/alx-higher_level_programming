ebce130474e30bfb3c9793d9eaf57852b89349ab
Charlyemex <charlyemex@gmail.com>
2023-09-13 11:52:50 -0700

    update

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
+const process = require('process');
+const message = 'No argument';
 if (process.argv[2] === undefined) {
-    console.log('No argument');
-  } else {
-    console.log(process.argv[2]);
-  }
+  console.log(message);
+} else {
+  console.log(process.argv[2]);
+}
